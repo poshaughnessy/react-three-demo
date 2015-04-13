846031fbed0043637dd764551db39b0f2a7da601
Peter O'Shaughnessy <peter.oshaughnessy@pearson.com>
2015-04-13 18:45:16 +0100

    Put react back to 0.12 until react-three is OK with 0.13. Use React in non-ES6 classes way for now.

--- src/components/app.js
+++ src/components/app.js
@@ -6,18 +6,17 @@ import React from 'react';
 //import ReactTHREE from 'react-three';
 //import ColladaLoader from '../../bower_components/threejs/examples/js/loaders/ColladaLoader';
 
-console.log('React', React);
-console.log('React.Component', React.Component);
-
 console.log('THREE', THREE);
 console.log('THREE.ColladaLoader', THREE.ColladaLoader);
 
-class AppComponent extends React.Component {
+// TODO put ES6 style back when react-three is upgraded to React v0.13 (https://github.com/Izzimach/react-three/issues/16)
+//class AppComponent extends React.Component {
+let AppComponent = React.createClass({
     render() {
         return (
             <h1>Hello</h1>
         );//ReactTHREE.Object3D()
     }
-}
+});
 
 export default AppComponent;
