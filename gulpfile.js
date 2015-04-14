6832601454da080aba8e26abf03a772fa5669484
Peter O'Shaughnessy <peter.oshaughnessy@pearson.com>
2015-04-14 17:05:32 +0100

    Use watchify for much faster browserify builds

--- gulpfile.js
+++ gulpfile.js
@@ -9,31 +9,30 @@ require('babel/register');
 // TEMP fix for this issue: https://github.com/babel/babel/issues/489
 Object.getPrototypeOf.toString = function() {return Object.toString();};
 
-var gulp = require('gulp'),
-    babel = require('gulp-babel'),
+var _ = require('lodash'),
+    gulp = require('gulp'),
+    //babel = require('gulp-babel'),
+    buffer = require('vinyl-buffer'),
     concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
+    gutil = require('gulp-util'),
+    //rename = require('gulp-rename'),
     sass = require('gulp-sass'),
+    source = require('vinyl-source-stream'),
     sourcemaps = require('gulp-sourcemaps'),
     babelify = require('babelify'),
     browserify = require('browserify'),
+    watchify = require('watchify'),
     fs = require('fs'),
     server = require('./server');
 
 /**
  *  Transpile and concatenate the JavaScripts into dist/bundle.js
  */
+/*
 gulp.task('babel', function() {
 
-    /**
-     * ,
-     shim: {
-                'three': { exports: 'global:THREE' },
-                'bower_components/threejs/examples/js/loaders/ColladaLoader.js': { depends: {'three': null}, exports: 'global:THREE.ColladaLoader' }
-            }
-     */
     return browserify('./src/main.js', {
-            debug: false
+            debug: true  // Set to false for production!
         })
         //.transform('browserify-shim')
         .transform(babelify.configure({
@@ -45,6 +44,63 @@ gulp.task('babel', function() {
         .pipe(fs.createWriteStream('./dist/bundle.js'));
 
 });
+*/
+
+/**
+ * Thanks rootical. Based on: https://gist.github.com/rootical/d700ea0d89bbfc362fc5
+ */
+function browserifyBuild(watch) {
+
+    var bundler;
+
+    if( watch ) {
+
+        bundler = watchify(browserify('./src/main.js',
+            _.assign(watchify.args, {
+                debug: true
+            })));
+
+        bundler.on('update', function() {
+
+            var hrTime = process.hrtime();
+            var t1 = hrTime[0] * 1000 + hrTime[1] / 1000000;
+
+            rebundle();
+
+            hrTime = process.hrtime();
+            var t2 = hrTime[0] * 1000 + hrTime[1] / 1000000;
+            gutil.log('Rebundle took ' + Math.round(t2-t1) + ' ms');
+
+        });
+
+    } else {
+
+        bundler = browserify('./src/main.js', {
+            debug: true
+        });
+
+    }
+
+    bundler.transform(babelify.configure({
+        compact: false
+    }));
+
+    function rebundle() {
+        return bundler.bundle()
+            .on('error', function(e) {
+                util.log('Browserify Error', e);
+            })
+            .pipe(source('bundle.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({
+                loadMaps: true
+            }))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('dist'));
+    }
+
+    return rebundle();
+}
 
 /**
  *  Compile and concatenate the SCSS files into dist/styles.css
@@ -60,29 +116,39 @@ gulp.task('sass', function() {
 
 });
 
+gulp.task('browserify', function() {
+    browserifyBuild(false);
+});
+
+/**
+ * Browserify watch
+ */
+gulp.task('browserify-watch', function() {
+    browserifyBuild(true);
+});
+
 /**
  * Compile JS and SCSS
  */
-gulp.task('compile', ['babel', 'sass'], function() {
+gulp.task('compile', ['browserify', 'sass'], function() {
 });
 
 /**
  * Compile and watch for changes
  */
-gulp.task('watch', ['compile'], function() {
-    gulp.watch('./src/**/*.js', ['babel']);
+gulp.task('watch', ['compile','browserify-watch'], function() {
     gulp.watch('./styles/*.scss', ['sass']);
 });
 
 /**
  * Compile and start watching, then start the server
  */
-gulp.task('server', ['watch'], function() {
+gulp.task('serve', ['watch'], function() {
     server.start();
 });
 
 /**
- * By default, runs the server task
+ * By default, runs the compile task
  */
-gulp.task('default', ['server'], function() {
+gulp.task('default', ['compile'], function() {
 });
--- src/components/app.js
+++ src/components/app.js
@@ -6,8 +6,8 @@ import React from 'react';
 //import ReactTHREE from 'react-three';
 //import ColladaLoader from '../../bower_components/threejs/examples/js/loaders/ColladaLoader';
 
-console.log('THREE', THREE);
-console.log('THREE.ColladaLoader', THREE.ColladaLoader);
+//console.log('THREE', THREE);
+//console.log('THREE.ColladaLoader', THREE.ColladaLoader);
 
 // TODO put ES6 style back when react-three is upgraded to React v0.13 (https://github.com/Izzimach/react-three/issues/16)
 //class AppComponent extends React.Component {
