58976fc02029d4da8f21f43b98b3a87d99f21379
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2015-04-25 19:42:50 +0100

    Trying to get JSONLoader model working...

--- src/components/robot.js
+++ src/components/robot.js
@@ -9,57 +9,78 @@ class RobotComponent extends React.Component {
 
         this.displayName = 'Robot';
 
-        // TODO put back when I figure this out
-        /*
-        /*
         this.state = {
             scene: null
         };
 
+        /*
         let colladaLoader = new THREE.ColladaLoader();
 
-        colladaLoader.load('/models/robby/RobbyTheRobot_FanArt.dae', collada => {
+        colladaLoader.load('/models/monster/monster.dae', collada => { //'/models/robby/RobbyTheRobot_FanArt.dae', collada => {
 
-            console.log('result', collada);
+            console.log('result scene', collada.scene);
 
-            self.setState({scene: collada.scene});
+            this.setState({scene: collada.scene});
 
-            self.props.onLoad();
+            this.props.onLoad();
         });
         */
 
-        this.texture = THREE.ImageUtils.loadTexture( '../../models/cupCake.png' );
-        this.material = new THREE.MeshBasicMaterial({ map: this.texture });
-        this.geometry = new THREE.BoxGeometry( 100, 100, 100 );
+        let loader = new THREE.JSONLoader();
+
+        console.log('Loading...');
+
+        loader.load('/models/robby/RobbyTheRobot_FanArt.js', (geometry, materials) => {
+
+            console.log('Loaded!', geometry, materials);
+
+            this.geometry = geometry;
+            this.material = new THREE.MeshFaceMaterial( materials );
+        });
+
+        //this.material = new THREE.MeshBasicMaterial({ color: 0xFF0000 });
+        //this.geometry = new THREE.BoxGeometry( 100, 100, 100 );
 
     }
 
     render() {
 
-        return React.createElement(
-            ReactTHREE.Object3D,
-            {position: this.props.position || new THREE.Vector3(0,0,0)},
-            React.createElement( ReactTHREE.Mesh, {geometry: this.geometry, material: this.material} )
-        );
-
-        // TODO put back when I figure this out
         /*
         if( this.state.scene ) {
 
-            return React.createElement(
-                ReactTHREE.Mesh,
-                {
-                    position: this.props.position || new THREE.Vector3(0, 0, 0),
-                    geometry: this.state.scene.geometry ? this.state.scene.geometry : new THREE.Geometry(),
-                    material: this.state.scene.material ? this.state.scene.material : new THREE.MeshFaceMaterial()
-                }
+            let children = [];
 
+            for( let i=0; i < this.state.scene.children.length; i++ ) {
+
+                var childObj = this.state.scene.children[i];
+
+                children.push(
+                    React.createElement( ReactTHREE.Mesh, {
+                        geometry: childObj.geometry,
+
+                    })
+                );
+
+            }
+
+            return React.createElement(
+                ReactTHREE.Object3D,
+                {},
+                children
             );
 
         } else {
-            return React.createElement(ReactTHREE.Object3D);
-        }
         */
+
+        return React.createElement(
+            ReactTHREE.Object3D,
+            {},
+            React.createElement( ReactTHREE.Mesh, {
+                geometry: this.geometry,
+                material: this.material,
+                position: this.props.position || new THREE.Vector3(0,0,0)
+            })
+        );
 
     }
 
