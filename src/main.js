cbcb01bed1bfe0b17fff0facf10f87cb600719cc
Peter O'Shaughnessy <peter.oshaughnessy@pearson.com>
2015-04-27 18:19:57 +0100

    Add basic animation

--- src/main.js
+++ src/main.js
@@ -2,13 +2,35 @@ import React from 'react';
 import AppComponent from './components/app';
 import THREE from 'three';
 
-let tempY = 0;
+const ROBOT_Z_NEAR = 50;
+const ROBOT_Z_FAR = 0;
+const ROBOT_MOVE_RATE = 0.1;
 
-let appState = { history: true, robotPosition: new THREE.Vector3(0,-25,0) };
+let appState = { history: true, robotPosition: new THREE.Vector3(0,-30,0), robotMovingForwards: true };
 
 function animate() {
-    //tempY++;
-    //appState.robotPosition.y = tempY;
+
+    let robotZ = appState.robotPosition.z;
+
+    if( appState.robotMovingForwards ) {
+
+
+        if( robotZ < 50 ) {
+            appState.robotPosition.z += ROBOT_MOVE_RATE;
+        } else {
+            appState.robotMovingForwards = false;
+        }
+
+    } else {
+
+        if( robotZ > 0 ) {
+            appState.robotPosition.z -= ROBOT_MOVE_RATE;
+        } else {
+            appState.robotMovingForwards = true;
+        }
+
+    }
+
     React.render(
         React.createElement( AppComponent, appState ),
         document.getElementById('app')
