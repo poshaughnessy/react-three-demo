6b574199e87484a22e168941bc78f26c0149d54e
Peter O'Shaughnessy <peter.oshaughnessy@pearson.com>
2015-05-15 14:48:21 +0100

    Tidying up

--- gulpfile.js
+++ gulpfile.js
@@ -11,41 +11,16 @@ Object.getPrototypeOf.toString = function() {return Object.toString();};
 
 var _ = require('lodash'),
     gulp = require('gulp'),
-    //babel = require('gulp-babel'),
     buffer = require('vinyl-buffer'),
     concat = require('gulp-concat'),
     gutil = require('gulp-util'),
-    //rename = require('gulp-rename'),
     sass = require('gulp-sass'),
     source = require('vinyl-source-stream'),
     sourcemaps = require('gulp-sourcemaps'),
     babelify = require('babelify'),
     browserify = require('browserify'),
-    browserifyShim = require('browserify-shim'),
     watchify = require('watchify'),
-    fs = require('fs'),
     server = require('./server');
-
-/**
- *  Transpile and concatenate the JavaScripts into dist/bundle.js
- */
-/*
-gulp.task('babel', function() {
-
-    return browserify('./src/main.js', {
-            debug: true  // Set to false for production!
-        })
-        //.transform('browserify-shim')
-        .transform(babelify.configure({
-            extensions: ['.js']//,
-            //ignore: 'bower_components'
-        }))
-        .bundle()
-        .on('error', function (err) { console.log('Babelify error : ' + err.message); })
-        .pipe(fs.createWriteStream('./dist/bundle.js'));
-
-});
-*/
 
 /**
  * Thanks rootical. Based on: https://gist.github.com/rootical/d700ea0d89bbfc362fc5
